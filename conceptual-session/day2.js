d2ceb23fe7f42d2d886adeadb02f1343c73c7f9c
dipongkor000 <dipongkorroy000@gmail.com>
2025-03-09 17:31:09 +0600

    add files

--- conceptual-session/day2.js
+++ conceptual-session/day2.js
@@ -1,9 +1,30 @@
-const cBtns = document.getElementsByClassName('ring-button');
-// console.log(ringButtons);
+const cBtns = document.getElementsByClassName("ring-button");
+// console.log(cBtns);
 
-for (let cBtn of cBtns){
-    cBtn.addEventListener('click', function (e) {
-        console.log(e.target);
-        e.target.classlist.add('border-purple-500')
-    });
+for (let cBtn of cBtns) {
+  cBtn.addEventListener("click", (e) => {
+    for (let c of cBtns) {
+      c.classList.remove("border-purple-600");
+    }
+    e.target.classList.add("border-purple-600");
+
+    const color = e.target.id.replace("-color", "");
+    const productImg = document.getElementById("product-image");
+    // productImg.src = "./images/gray.png";
+    productImg.src = "./images/" + color + ".png";
+  });
 }
+
+const selectWristSize = (model) => {
+  const sizes = ["S", "M", "L", "XL"];
+  for (let element of sizes) {
+    const btn = document.getElementById("size-" + element);
+    if (element === model) {
+      btn.classList.add("border-purple-600");
+    } else {
+      btn.classList.remove("border-purple-600");
+    }
+  }
+};
+
+
